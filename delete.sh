213c29afffa25778d6677409518bcff1b4f95ab2
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-03-30 23:53:15 +0400

    Fixed a few typos

--- delete.sh
+++ delete.sh
@@ -5,6 +5,27 @@ if [ $# = 0 ]; then
 	echo "No arguments provided"
 
 
+elif [ $1 = 'instance' ]; then
+
+INSTANCE_ID=\
+$(aws ec2 describe-instances \
+	--filters "Name=instance-state-name,Values=running" "Name=tag:DeleteMe,Values=Yes" \
+	--query 'Reservations[*].Instances[*].[InstanceId]' \
+	--output text)
+
+aws ec2 stop-instances \
+	--instance-id $INSTANCE_ID \
+	--output text > /dev/null
+
+echo "$INSTANCE_ID stopped"
+
+aws ec2 terminate-instances \
+	--instance-id $INSTANCE_ID \
+	--output text > /dev/null
+
+echo "$INSTANCE_ID terminated"
+
+
 elif [ $1 = 'vpc' ]; then
 
 SG_ID=\
@@ -67,26 +88,5 @@ aws ec2 delete-vpc \
 	--vpc-id $VPC_ID
 
 echo "$VPC_ID deleted"
-
-
-elif [ $1 = 'instance' ]; then
-
-INSTANCE_ID=\
-$(aws ec2 describe-instances \
-	--filters "Name=instance-state-name,Values=running" "Name=tag:DeleteMe,Values=Yes" \
-	--query 'Reservations[*].Instances[*].[InstanceId]' \
-	--output text)
-
-aws ec2 stop-instances \
-	--instance-id $INSTANCE_ID \
-	--output text > /dev/null
-
-echo "$INSTANCE_ID stopped"
-
-aws ec2 terminate-instances \
-	--instance-id $INSTANCE_ID \
-	--output text > /dev/null
-
-echo "$INSTANCE_ID terminated"
 
 fi
