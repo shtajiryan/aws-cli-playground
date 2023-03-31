78141f7e676cd3f01224cc08b62bbe64670375c5
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-03-31 23:43:40 +0400

    fixed life-threatening typos

--- cron_job.sh
+++ cron_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i ~/Downloads/shtajiryan.pem ubuntu@$1 'echo "* * * * * date > /var/www/html/index.nginx-debian.html" > cron.tmp'
-ssh -i ~/Downloads/shtajiryan.pem ubuntu@$1 'echo "* * * * * sleep 30; date > /var/www/html/index.nginx-debian.html" >> cron.tmp'
-ssh -i ~/Downloads/shtajiryan.pem ubuntu@$1 'crontab cron.tpm'
-ssh -i ~/Downloads/shtajiryan.pem ubuntu@$1 'rm cron.tpm'
+ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'echo "* * * * * date > /var/www/html/index.nginx-debian.html" > cron.tmp'
+ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'echo "* * * * * sleep 30; date > /var/www/html/index.nginx-debian.html" >> cron.tmp'
+ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'crontab ~/cron.tmp'
+ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'rm ~/cron.tmp'
