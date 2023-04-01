e486e3d1844c99b2692c62e48cbcb51356d3a1a2
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-04-01 11:20:31 +0400

    removed some comments

--- cron_job.sh
+++ cron_job.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'echo "* * * * * date > /var/www/html/index.nginx-debian.html" > cron.tmp'
-# ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'echo "* * * * * sleep 30; date > /var/www/html/index.nginx-debian.html" >> cron.tmp'
-# ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'crontab ~/cron.tmp'
-# ssh -i ~/Downloads/shtajiryan.pem ubuntu@$IP 'rm ~/cron.tmp'
-
-
 ssh -i $key ubuntu@$IP '
     echo "* * * * * date > /var/www/html/index.nginx-debian.html" > cron.tmp
     echo "* * * * * sleep 30; date > /var/www/html/index.nginx-debian.html" >> cron.tmp
