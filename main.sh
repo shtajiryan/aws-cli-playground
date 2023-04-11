f8ab5fef4d57d325a5ab90c9e25db5b4cf43754c
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-04-12 01:55:16 +0400

    finished creation with error checking, started deletion

--- main.sh
+++ main.sh
@@ -1,33 +1,64 @@
 #!/bin/bash
 
 source ./utils/vpc.sh
+source ./utils/instance.sh
+
+case ${1} in
+
+create)
 
 create_vpc
 
 if [ -z "$VPC_ID" ]; then
     echo "Won't create the rest of resources as VPC ID is empty" && exit
-
 else
+
 create_subnet
 fi
 
 if [ -z "$SUBNET_ID" ]; then
     echo "Won't create the rest of resources as Subnet ID is empty" && exit
-
 else
+
 create_igw
 fi
 
 if [ -z "$IGW_ID" ]; then
     echo "Won't create the rest of resources as Internet Gateway ID is empty" && exit
-
 else
-create_rt
 
+create_rt
 fi
+
 if [ -z "$RT_ID" ]; then
-    echo "Won't create the rest of resources as Route table ID is empty" && exit
+    echo "Won't create the rest of resources as Route Table ID is empty" && exit
 
 else
     echo "All VPC resources created!"
+
+create_sg
 fi
+
+if [ -z "$SG_ID" ]; then
+    echo "Won't create an instance as Security Group ID is empty" && exit
+else
+
+KEY_PAIR=shtajiryan
+
+create_instance
+fi
+
+if [ -z "$INSTANCE_ID" ]; then
+    echo "Instance not created" && exit
+else
+    echo "Instance with IP address $PUBLIC_IP created!"
+fi
+
+;;
+delete)
+
+delete_instance
+delete_sg
+
+;;
+esac
